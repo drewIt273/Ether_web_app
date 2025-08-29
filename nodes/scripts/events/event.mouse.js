29149e33abb6e40e3dcc72147477ee9d592b1c7b
DrewIt273 <instantdrewIt@gmail.com>
2025-08-29 22:53:15 +0100

    Adding a sticky state to the sidebar

--- nodes/scripts/events/event.mouse.js
+++ nodes/scripts/events/event.mouse.js
@@ -6,31 +6,43 @@
 
 import {dom} from "../domquery.js"
 
+/**
+ * Sidebar local function handler
+ */
 !function() {
-    var h = dom("[aside-state]").at(0), d = dom("#h01").at(0), e = dom("aside#sidebar-nav").at(0), f = (s) => h.setAttribute("aside-state", s), SP = (e, p, v) => e.style.setProperty(p, v);
+    var h = dom("[aside-state]").at(0), d = dom("#h01").at(0), e = dom("aside#sidebar-nav").at(0), da = dom('[data-event="toggle-aside-state"]').at(0), sno = dom('#sidebar-nav [data-events-effects="opacity"]'), snoh = n => {SP(n, "opacity", 0); setTimeout(() => {SP(n, "display", "none")}, 340)}, ns = (n) => {n.style = ``}, f = (s) => h.setAttribute("aside-state", s), SP = (e, p, v) => e.style.setProperty(p, v), A = (e, a) => e.getAttribute(a), epf = () => {d.classList.add("items-center"); SP(d, "width", "25px")}, nepf = () => {d.classList.remove("items-center")};
 
     dom("[event-click]")
+
         .delegate("click", () => {
-            var a = h.getAttribute("aside-state")
-            if (a === "fixed") {
-                f("absolute"); SP(d, "width", "10px"); SP(e, "margin-left", "")
+            var a = A(h, "aside-state")
+            if (a !== "sticky") {
+                if (a == "fixed") {
+                    f("absolute"); SP(d, "width", "10px"); SP(e, "margin-left", ""); da.firstElementChild.innerHTML = `<span id="n02"></span>`
+                }
+                if (a == "absolute") {
+                    f("fixed"); SP(d, "width", "320px"); SP(e, "margin-left", ""); da.firstElementChild.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="var(--svg-color)" aria-hidden="true" data-slot="icon" class="svg-size"><path stroke-linecap="round" stroke-linejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"></path></svg>`
+                }
             }
-            if (a === "absolute") {
-                f("fixed"); SP(d, "width", "320px"); SP(e, "margin-left", "")
-            }
+            nepf(), sno.nodes.forEach(node => {ns(node)})
         }, '[data-event="toggle-aside-state"]')
+
+        .delegate("click", () => {
+            sno.nodes.forEach(node => {snoh(node)})
+            f("sticky"); setTimeout(() => {SP(e, "width", "40px")}, 570); epf()
+        }, '[data-event="toggle-stickybar-state"]')
 
     dom("[event-hover]")
 
         .delegate("mouseleave", () => {
-            var a = h.getAttribute("aside-state")
+            var a = A(h, "aside-state")
             if (a === "absolute") {
                 SP(e, "margin-left", "-325px")
             }
         }, "#h01")
 
         .delegate("mouseenter", () => {
-            var a = h.getAttribute("aside-state")
+            var a = A(h, "aside-state")
             if (a === "absolute") {
                 SP(e, "margin-left", "10px")
             }
