fbf8b1a4ecc1b8374f99b59962a2dccb07cfea6f
DrewIt273 <instantdrewIt@gmail.com>
2025-09-20 10:20:54 +0100

    Including utility functions and more that may be used frequently

--- nodes/scripts/utilities/any.js
+++ nodes/scripts/utilities/any.js
@@ -4,4 +4,47 @@
  * any.js
  */
 
-/** */
+/**
+ * Converts camelCase or pascalCase to kebab-case: myAttrName -> my-attr-name
+ * @param {string} s 
+ */
+export function toKebab(s) {
+    return String(s).replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+}
+
+export const isNode = v => v instanceof Node
+
+/** @param {Node} v */
+export const hasNode = v => isNode(v) && document.contains(v)
+
+/** @param {string} v */
+export const isString = v => typeof v === "string"
+
+/**
+ * Returns the computed style property of an element
+ * @param {Element} e @param {string} p 
+ */
+export const getStyle = (e, p) => getComputedStyle(e)?.[p]
+
+/**
+ * Set an inline property style of an element
+ * @param {Element} e Element @param {String} p CSSProperty @param {String} v CSSValue
+ */
+export const setStyle = (e, p, v) => e.style.setProperty(p, v)
+
+/**
+ * @param {string | Node} s 
+ */
+export const find = s => isString(s) ? document.querySelector(s) : hasNode(s) ? s : null
+
+/**
+ * @param {Element} s 
+ */
+export const findAll = s => isString(s) ? Array.from(document.querySelectorAll(s)) : hasNode(s) ? s : null
+export const web_app = find('web-app')
+
+/**
+ * Removes event listeners from nodes in the DOM
+ * @param {NodeListOf<Node>} nodes should be a query result @param {string} ev @param {Function} handler 
+ */
+export const removeListeners = (nodes, ev, handler) => Array.from(nodes).forEach(node => {node.removeEventListener(ev, handler)})
