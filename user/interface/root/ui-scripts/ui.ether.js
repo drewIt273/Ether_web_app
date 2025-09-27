1359bfd68e4f7e0a9f3d7a37d8807a0fa3f25505
DrewIt273 <instantdrewIt@gmail.com>
2025-09-27 14:14:02 +0100

    Adding attributes-handling methods. Adjusting imports

--- user/interface/root/ui-scripts/ui.ether.js
+++ user/interface/root/ui-scripts/ui.ether.js
@@ -4,14 +4,13 @@
  * ui.ether.js
  */
 
-import {isNode, isString, create, find} from "../../../../nodes/scripts/utilities/any.js";
+import {isNode, isString, create, find, toKebab, setAttr, hasAttr, removeAttr, dataset} from "../../../../nodes/scripts/utilities/any.js";
 import {div} from "../../../../nodes/scripts/nodecreator.js";
 
 class UIComponent {
 
     /**
-     * @param {string|Element} node 
-     * @param {HTMLElement[]} append 
+     * @param {string|HTMLElement} node @param {HTMLElement[]} append 
      */
     constructor(node, append = []) {
         this.node = isString(node) ? create(node) : isNode(node) ? node : new div
@@ -49,8 +48,47 @@ class UIComponent {
         return isString(s) ? this.node.querySelector(s) : null
     }
 
+    /**
+     * @param {string|Node} parent 
+     */
     mount(parent) {
         (find(parent) || document.body).appendChild(this.node)
+        return this
+    }
+
+    attrs(obj = {}) {
+        for (const [prop, val] of Object.entries(obj)) {
+            setAttr(this.node, toKebab(prop), val)
+        }
+        return this
+    } 
+
+    /**
+     * @param {string} attr 
+     */
+    getAttr(attr) {
+        return this.node.getAttribute(attr)
+    }
+
+    /**
+     * @param {string} a 
+     */
+    hasAttr(a) {
+        return hasAttr(this.node, a)
+    }
+
+    /**
+     * @param {string} a 
+     */
+    removeAttr(a) {
+        removeAttr(this.node, a)
+        return this
+    }
+    /**
+     * @param {string} attr @param {string} val 
+     */
+    dataset(attr, val) {
+        dataset(this.node, attr, val)
         return this
     }
 }
