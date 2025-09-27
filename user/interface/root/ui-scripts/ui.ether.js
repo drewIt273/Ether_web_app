2d52ebcf4d36a0f90ee3932f00007c06ebf2f10e
DrewIt273 <instantdrewIt@gmail.com>
2025-09-27 13:42:57 +0100

    Adding few methods to the class.

--- user/interface/root/ui-scripts/ui.ether.js
+++ user/interface/root/ui-scripts/ui.ether.js
@@ -4,16 +4,53 @@
  * ui.ether.js
  */
 
-import {isNode, isString, create} from "../../../../nodes/scripts/utilities/any.js";
+import {isNode, isString, create, find} from "../../../../nodes/scripts/utilities/any.js";
 import {div} from "../../../../nodes/scripts/nodecreator.js";
 
 class UIComponent {
 
     /**
-     * @param {Node|string} node 
+     * @param {string|Element} node 
      * @param {HTMLElement[]} append 
      */
-    constructor(node, append) {
-        this.Node = isString(node) ? create(node) : isNode(node) ? node : new div
+    constructor(node, append = []) {
+        this.node = isString(node) ? create(node) : isNode(node) ? node : new div
+        for (const e of append) {
+            this.node.appendChild(e)
+        }
+    }
+
+    get children() {
+        return Array.from(this.node.childNodes)
+    }
+
+    /**
+     * @param {...Node} nodes
+     */
+    append(...nodes) {
+        nodes.forEach(n => {
+            if (n instanceof Node) this.node.appendChild(n)
+        })
+        return this
+    }
+
+    /**
+     * @param {Function} callback 
+     */
+    forEach(callback) {
+        this.children.forEach(callback)
+        return this
+    }
+
+    /**
+     * @param {string} s 
+     */
+    find(s) {
+        return isString(s) ? this.node.querySelector(s) : null
+    }
+
+    mount(parent) {
+        (find(parent) || document.body).appendChild(this.node)
+        return this
     }
 }
