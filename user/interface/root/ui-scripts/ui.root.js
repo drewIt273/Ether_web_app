992d8f0096159e6fa9ff22bc5cf524000172dd99
DrewIt273 <instantdrewIt@gmail.com>
2025-11-03 21:06:25 +0100

    Importing getStyle.

--- user/interface/root/ui-scripts/ui.root.js
+++ user/interface/root/ui-scripts/ui.root.js
@@ -4,7 +4,7 @@
  * ui.root.js
  */
 
-import {isNode, isString, create, find, findAll, toKebab, setAttr, hasAttr, removeAttr, dataset, on, off, ranstring, strictObject, removeNode, setStyle, Registry, isArray} from "../../../../nodes/scripts/utilities/any.js";
+import {isNode, isString, create, find, findAll, toKebab, setAttr, hasAttr, removeAttr, dataset, on, off, ranstring, strictObject, removeNode, setStyle, Registry, isArray, getStyle} from "../../../../nodes/scripts/utilities/any.js";
 import {div} from "../../../../nodes/scripts/nodecreator.js";
 import {stylesheet} from "../../../../nodes/scripts/stylesheet.js";
 
