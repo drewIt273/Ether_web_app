a6fe1db6b3c08a52c7b65a0d3148b9ce613876fb
DrewIt273 <instantdrewIt@gmail.com>
2025-11-21 19:30:15 +0100

    initializing the dom_module

--- root/core/dom.js
+++ root/core/dom.js
@@ -4,3 +4,12 @@
  * dom.js
  */
 
+class dom_module {
+    constructor() {
+        /* All the functions and stuff defined in this file
+         * shall be stored as properties and methods of this class. 
+         */
+    }
+}
+
+export const dom = dom_module;
