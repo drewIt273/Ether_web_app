90158dd40c1bedf3917171bb9207433bed77fbd3
DrewIt273 <instantdrewIt@gmail.com>
2025-11-21 22:33:34 +0100

    little changes.

--- user/interface/root/ui-scripts/ui.root.js
+++ user/interface/root/ui-scripts/ui.root.js
@@ -7,6 +7,7 @@
 import {isNode, isString, create, find, findAll, toKebab, setAttr, hasAttr, removeAttr, dataset, on, off, ranstring, strictObject, removeNode, setStyle, Registry, isArray, getStyle} from "../../../../nodes/scripts/utilities/any.js";
 import {div} from "../../../../nodes/scripts/nodecreator.js";
 import {stylesheet} from "../../../../nodes/scripts/stylesheet.js";
+import {runtime} from "../../../../root/core/runtime.js";
 
 export const ActiveUICells = new Registry;
 export const ActiveUIBlocks = new Registry;
