80ea8dd1b530625d8d6f2ae846db952766597e89
DrewIt273 <instantdrewIt@gmail.com>
2025-11-21 19:32:53 +0100

    initializing the app's kernel. runtime is the main instance of the kernel on which the app will be built.

--- root/core/runtime.js
+++ root/core/runtime.js
@@ -5,3 +5,14 @@
  * co-built with GPT-5
  */
 
+import {dom} from './dom.js'
+
+class Kernel {
+    constructor() {
+        /**Returns the modules constructors */
+        this.modules = {dom};
+        this.dom = new dom;
+    }
+}
+
+export const runtime = new Kernel;
