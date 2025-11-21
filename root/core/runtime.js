7d48bddb2e814b386c74f47d3d310b4f8856ad23
DrewIt273 <instantdrewIt@gmail.com>
2025-11-21 21:03:08 +0100

    feat(runtime): implement Kernel skeleton with module instantiation, async boot, and lifecycle hook handling

--- root/core/runtime.js
+++ root/core/runtime.js
@@ -12,14 +12,40 @@ class Kernel {
         /**Returns the modules constructors */
         this.modules = {dom};
         this.dom = new dom(this);
+        this.hooks = {
+            init: [],
+            ready: []
+        }
+        this.ready = !1;
+        this.init = !1;
     }
 
     async boot() {
         await this.#runstartupHooks()
     }
 
-    #runstartupHooks() {
-        // Will call lifecycle hooks.
+    async #runstartupHooks() {
+        for (const fn of this.hooks.init) await fn.call(this);
+        this.init = !0
+        for (const fn of this.hooks.ready) await fn.call(this);
+        this.ready = !0
+    }
+
+    /**
+     * 
+     * @param {"ready"|"init"} st 
+     * @param {()} fn 
+     */
+    on(st, fn) {
+        switch(st) {
+            case 'ready':
+                this.ready ? fn.call(this) : this.hooks.ready.push(fn)
+                break
+                
+            case 'init':
+                this.init ? fn.call(this) : this.hooks.init.push(fn)
+                break
+        }
     }
 }
 
