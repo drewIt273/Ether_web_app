4056886e52647ed3058f4d45d85467b67d3316e0
DrewIt273 <instantdrewIt@gmail.com>
2025-12-14 12:27:57 +0100

    Initializing properties.

--- root/core/events.js
+++ root/core/events.js
@@ -4,8 +4,21 @@
  * events.js
  */
 
+import {Registry} from "../../nodes/scripts/any.js"
+
 export class events_module {
     constructor(/**@type {kernel}*/runtime) {
-        this.runtime = runtime
+        /** Reference to the kernel for hooks, module communication, etc. */
+            this.runtime = runtime
+        /**A registry for nodes still having active listeners in the DOM */
+            this.ActiveListeners = new Registry()
+        /**A registry for nodes whose event listeners where removed from the DOM */
+            this.BacklogListeners = new Registry()
+        /**Track which global events are already delegated @type {Set<string>} */
+            this.ActiveGlobals = new Set()
+        /**Stores references to global delegated listeners so they can be removed later @type {Map<string, ()>} */
+            this.GlobalDelegates = new Map()
     }
+
+    listen(ev, target, ...handlers) {    }
 }
