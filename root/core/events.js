bf7a68764d02d468513df6ef5d6968437375fee4
DrewIt273 <instantdrewIt@gmail.com>
2025-12-16 20:41:08 +0100

    Implementing keybind method.

--- root/core/events.js
+++ root/core/events.js
@@ -5,6 +5,7 @@
  */
 
 import {find, isNode, isString, Registry} from "../../nodes/scripts/any.js"
+import {UIBlock, UICell, UIComponent, UIConstructorOf} from "../assets/ui-root.js"
 
 export class events_module {
     constructor(/**@type {kernel}*/runtime) {
@@ -18,6 +19,8 @@ export class events_module {
             this.ActiveGlobals = new Set()
         /**Stores references to global delegated listeners so they can be removed later @type {Map<string, ()>} */
             this.GlobalDelegates = new Map()
+        /**@type {WeakMap<(UICell|UIBlock|UIComponent), [string[], ()][]>} */
+            this.Keybinds = new WeakMap()
         /**@type {Element} */
             this.root = runtime.dom.find('#mainLayoutContainer')
     }
@@ -94,5 +97,13 @@ export class events_module {
             // If no handlers left, remove from ActiveListeners
                 if (!existing.fn.length) a.splice(0, a.size, a.filter(o => o !== existing))
         })
+    }
+
+    /**
+     * @param {("ctrl"|"alt"|"meta"|"shift")[]} keys @param {UICell|UIBlock|UIComponent} node @param {()} handler 
+     */
+    keybind(keys, node, handler) {
+        if (typeof handler !== 'function' || UIConstructorOf(node?.node) === undefined) return;
+        else this.Keybinds.has(node.node) ? this.Keybinds.get(node.node).push([keys, handler]) : this.Keybinds.set(node.node, [[keys, handler]])
     }
 }
