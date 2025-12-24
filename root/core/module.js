09b360cf1988acdbe31650eff776406ce4ca3ac0
DrewIt273 <instantdrewIt@gmail.com>
2025-12-24 14:36:14 +0100

    Importing Kernel (temporaly) and initializing default properties.

--- root/core/module.js
+++ root/core/module.js
@@ -4,9 +4,15 @@
  * module.js
  */
 
+import {Kernel} from "./runtime.js"
+
 export class KModule {
     constructor(/**@type {Kernel}*/ runtime) {
         this.runtime = runtime
+        /**@type {Map<any, ()>} */
+        this.mappedData = new Map()
+        this.received = null
+        this.sentdata = null
     }
 }
 export const isModule = v => v instanceof KModule
