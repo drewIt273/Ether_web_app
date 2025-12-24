e9175fb663046de8c8b14a15c1d8909073cb8634
DrewIt273 <instantdrewIt@gmail.com>
2025-12-24 15:05:10 +0100

    Writing the emit method that all kernel modules shall use to communicate.

--- root/core/module.js
+++ root/core/module.js
@@ -14,5 +14,26 @@ export class KModule {
         this.received = null
         this.sentdata = null
     }
+
+    emit(data) {
+        return {
+            /**
+             * @param {KModule} target 
+             * @param {(target: KModule, data?: *)} callback
+             */
+            to: (target, callback) => {
+                if (isModule(target)) {
+                    this.runtime.interface.emit(data).to(target, callback)
+                    this.sentdata = data
+                }
+            },
+            /**
+             * @param {(target: KModule, data?: *)} c 
+             */
+            map: (c) => {
+                this.mappedData.set(data, c)
+            }            
+        }
+    }
 }
 export const isModule = v => v instanceof KModule
