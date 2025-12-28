8186f007dc54a5ef100a94e2ea987a28aa0f002b
DrewIt273 <instantdrewIt@gmail.com>
2025-12-28 12:18:19 +0100

    Initializing KModule constructor

--- root/core/module.js
+++ root/core/module.js
@@ -7,6 +7,12 @@
 import {Kernel} from "./runtime.js"
 
 export class KModule {
-    
+    constructor(/**@type {Kernel}*/ r) {
+        this.runtime = r
+        /**@type {Map<*, ()>} */
+        this.mappedData = new Map()
+        this.receivedData = null
+        this.sentData = null
+    }
 }
 export const isModule = v => v instanceof KModule
