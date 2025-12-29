6e2ededb18b9fe15b209ba612679ff809c1b2867
DrewIt273 <instantdrewIt@gmail.com>
2025-12-29 14:01:18 +0100

    The component lifecycle should not be a single module in the runtime but broken down everywhere in the runtime to perform actions only when conditions are met.

--- root/core/runtime.js
+++ root/core/runtime.js
@@ -6,17 +6,15 @@
  */
 
 import {dom_module} from './dom.js'
-import {comp_module} from './comp.js'
 import {events_module} from './events.js'
 import {state_module} from './state.js'
-import {isModule, KModule} from './module.js';
+import {isModule, KModule} from './module.js'
 
 export class Kernel {
     constructor() {
         /**Returns the modules constructors */
-        this.modules = {dom_module, comp_module, events_module, state_module};
+        this.modules = {dom_module, events_module, state_module};
         this.dom = new dom_module(this);
-        this.lifecycle = new comp_module(this);
         this.events = new events_module(this);
         this.state = new state_module(this);
         this.hooks = {
