bce35c3ebd80ade3a7326fc5ffbc302356c6fea0
DrewIt273 <instantdrewIt@gmail.com>
2026-01-17 18:35:04 +0100

    nothing important.

--- root/core/dom.js
+++ root/core/dom.js
@@ -7,7 +7,7 @@
 import {jsx} from "../assets/nodecreator.js";
 import {query$} from "../assets/domquery.js";
 import {Registry} from "../../nodes/scripts/any.js";
-import {UICell as cell, UIBlock as block, UIComponent as comp, UIConstructorOf as fu, UIreg} from "../assets/ui-root.js";
+import {UICell as cell, UIBlock as block, UIComponent as comp, UIConstructorOf as fu} from "../assets/ui-root.js";
 import {DModule} from "./module.js";
 
 const doc = window.document;
