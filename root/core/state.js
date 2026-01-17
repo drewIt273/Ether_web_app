ad2ac81f2dd4a319944454817710e3c411a23e14
DrewIt273 <instantdrewIt@gmail.com>
2026-01-17 17:44:33 +0100

    Initializing basic properties and methods.

--- root/core/state.js
+++ root/core/state.js
@@ -10,5 +10,15 @@ export class StateManager extends DModule {
 
     constructor(runtime) {
         super(runtime)
+        this.init = !1
+        this.ready = !1
+    }
+
+    async onInit() {
+        this.init = !0
+    }
+
+    async onReady() {
+        this.ready = !0
     }
 }
