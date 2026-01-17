bae6796647ff6636d8e0f1e9f1db1107ae09a29a
DrewIt273 <instantdrewIt@gmail.com>
2026-01-17 18:37:36 +0100

    Defining basic methods for defining and setting UINodes' states.

--- root/core/state.js
+++ root/core/state.js
@@ -24,4 +24,25 @@ export class StateManager extends DModule {
     async onReady() {
         this.ready = !0
     }
+
+    /**
+     * @param {UINode} node @param {string} state
+     */
+    define(node, state, fn = () => {}) {
+        !this.reg.has(node.node) ? this.reg.set(node.node, {[state]: fn}) : this.reg.get(node.node)[state] = fn
+        node.dataset({state: state})
+    }
+
+    /**
+     * @param {UINode} node @param {string} state 
+     */
+    set(node, state) {
+        if (this.reg.has(node))
+            if (Object.hasOwn(this.reg.get(node.node), state)) {
+                node.dataset({state: state})
+                this.reg.get(node.node)[state].call(node)
+            }
+            else throw new Error(`State '${state}' not defined for UINode '${node}'`)
+        else throw new Error(`[StateManager] UINode '${node}' not registered`)
+    }
 }
