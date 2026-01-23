d4f1615a66993d70ac8a0f38d92c81cd3722abde
DrewIt273 <instantdrewIt@gmail.com>
2026-01-23 20:47:09 +0100

    Creating store.js, the storage center of Ether

---
+++ root/core/store.js
@@ -0,0 +1,14 @@
+/**
+ * Instance by DrewIt
+ * 
+ * store.js
+ */
+
+import {DModule} from "./module.js"
+
+export class StoreModule extends DModule {
+
+    constructor(runtime) {
+        super(runtime)
+    }
+}
