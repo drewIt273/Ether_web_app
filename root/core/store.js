28caa81679b3b77754232b4159db6f28be04527e
DrewIt273 <instantdrewIt@gmail.com>
2026-01-25 16:38:06 +0100

    Initializing helper function 'stringify'.

--- root/core/store.js
+++ root/core/store.js
@@ -8,6 +8,10 @@ import {DModule} from "./module.js"
 
 const local = localStorage, session = sessionStorage, indexed = indexedDB
 
+function stringify(v) {
+    return v?.key ? v.key : JSON.stringify(v)
+}
+
 export class StorageManager extends DModule {
 
     constructor(runtime) {
