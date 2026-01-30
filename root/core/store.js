a7e83118debf146ec4f796456f0755fafc65184b
DrewIt273 <instantdrewIt@gmail.com>
2026-01-30 20:09:13 +0100

    Setting an object to manage the localStorage

--- root/core/store.js
+++ root/core/store.js
@@ -17,4 +17,32 @@ export class StorageManager extends DModule {
     constructor(runtime) {
         super(runtime)
     }
+
+    local = {
+        /**
+         * @param {number} i 
+         */
+        key: i => {
+            return local.key(i)
+        },
+        /**
+         * @param {string} k @param {*} v 
+         */
+        set: (k, v) => {
+            local.setItem(k, JSON.stringify(v))
+        },
+        /**
+         * @param {string} k 
+         */
+        get: k => {
+            return JSON.parse(local.getItem(k))
+        },
+        /**
+         * @param {string} k 
+         */
+        undo: k => {
+            local.removeItem(k)
+        },
+        size: local.length,
+    }
 }
