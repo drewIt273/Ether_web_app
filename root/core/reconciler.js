a9cf843e1d1df53a766dfa2ffcba247267200189
DrewIt273 <instantdrewIt@gmail.com>
2026-02-26 20:47:56 +0100

    Setting async onInit and onReady functions.

--- root/core/reconciler.js
+++ root/core/reconciler.js
@@ -9,4 +9,12 @@ export class Reconciler extends DModule {
     constructor(runtime) {
         super(runtime)
     }
+
+    async onInit() {
+        this.init = !0
+    }
+
+    async onReady() {
+        this.ready = !0
+    }
 }
