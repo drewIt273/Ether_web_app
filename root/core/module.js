4dc2cee70a294362b375da25c8399f8337c020fa
DrewIt273 <instantdrewIt@gmail.com>
2026-03-08 15:34:59 +0100

    Refactor DModule to use IMC for emitting data to runtime modules

--- root/core/module.js
+++ root/core/module.js
@@ -6,6 +6,16 @@
 
 import {Kernel} from "./runtime.js"
 
+const IMC = {
+    emit: (data) => (t, ...args) => {
+        if (isModule(t)) try {
+            t.rd = data
+            if (t.md.has(data)) t.md.get(data).call(t, ...args)
+        } catch {}
+        else throw new TypeError(`${t} is not a runtime module`)
+    },
+}
+
 export class DModule {
     constructor(/**@type {Kernel}*/ r) {
         this.runtime = r
@@ -22,8 +32,10 @@ export class DModule {
              * @param {DModule} t 
              */
             to: (t, ...args) => {
-                this.sd = data
-                this.runtime.interface.emit(data).to(t, ...args)
+                if (isModule(t)) {
+                    this.sd = data
+                    IMC.emit(data)(t, ...args)
+                }
                 return this.emit
             },
             /**
