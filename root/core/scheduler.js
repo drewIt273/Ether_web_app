7daedf07917fd88b04bbba706604ca1c83d7b587
DrewIt273 <instantdrewIt@gmail.com>
2026-04-11 13:43:36 +0100

    Improving scheduler with methods to handle jobs mapped to UINodes states.

--- root/core/scheduler.js
+++ root/core/scheduler.js
@@ -12,6 +12,7 @@ export class Scheduler extends DModule {
         this.frameQueue = new Set
         this.microPending = !1
         this.framePending = !1
+        this.emit('sch').map(f => this.schedule(f))
     }
 
     async onInit() {
@@ -20,5 +21,35 @@ export class Scheduler extends DModule {
 
     async onReady() {
         this.ready = !0
+    }
+
+    /**
+     * @param {()} job
+     */
+    schedule(job) {
+        this.microQueue.add(job)
+        if (!this.microPending) {
+            this.microPending = !0
+            queueMicrotask(() => this.flushMicro())
+        }
+    }
+
+    flushMicro() {
+        for (const job of this.microQueue) this.frameQueue.add(job)
+        this.microQueue.clear()
+        this.microPending = !1
+        this.scheduleFrame()
+    }
+
+    scheduleFrame() {
+        if (this.framePending) return;
+        this.framePending = !0
+        requestAnimationFrame(() => this.flushFrame())
+    }
+
+    flushFrame() {
+        for (const job of this.frameQueue) job()
+        this.frameQueue.clear()
+        this.framePending = !1
     }
 }
