4a79d5a7e9140dd3d93432d019af174d712720e7
DrewIt273 <instantdrewIt@gmail.com>
2026-06-14 15:21:41 +0100

    Initializing ui-root.ts

---
+++ src/runtime/dom/ui-root.ts
@@ -0,0 +1,43 @@
+/**
+ * Instance by DrewIt
+ */
+
+import {Registry} from "@assets/registry";
+import {toKebab} from "@assets/any";
+
+export const UIReg = new Registry;
+
+export class UINode {
+
+    node: HTMLElement
+    parent: ParentNode | null
+    childNodes: ChildNode[]
+    constructor(n: string|HTMLElement = 'div') {
+        this.node = n instanceof HTMLElement ? n : document.createElement(n)
+        this.parent = this.node.parentNode
+        this.childNodes = Array.from(this.node.childNodes)
+    }
+
+    get key() {
+        return this.node?.getAttribute('ui-data-key')
+    }
+
+    set UIKey(s: string) {
+        this.attrs({'ui-data-key': s})
+    }
+
+    attrs(o: Record<string, string>) {
+        for (const [k, v] of Object.entries(o)) this.node.setAttribute(toKebab(k), v)
+        return this
+    }
+
+    dataset(o: Record<string, string>) {
+        for (const [k, v] of Object.entries(o)) this.node.setAttribute(toKebab(`data-${k}`), v)
+        return this
+    }
+
+    style(o: Record<string, any>) {
+        for (const [k, v] of Object.entries(o)) Object.assign(this.node.style, {[toKebab(k)]: v})
+        return this
+    }
+}
