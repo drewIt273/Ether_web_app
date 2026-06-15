59e3a5cbba9c7948243d3714c2e7c76e48b324ac
DrewIt273 <instantdrewIt@gmail.com>
2026-06-16 00:25:00 +0100

    Implementing NodeMessageResolver that Cells and Blocks will use for communicating.

--- src/runtime/dom/dom.ts
+++ src/runtime/dom/dom.ts
@@ -10,3 +10,35 @@ export class DOMInterface extends Module {
         super()
     }
 }
+
+interface Sender {
+    emittedData?: any
+}
+interface Receiver {
+    mappedData: Map<any, HandlerList>
+}
+
+function NodeMessageResolver(sender: CellOrBlock, receiver: CellOrBlock) {
+    const call = (h: HandlerList, ...args: any[]) => {
+        for (const fn of h) fn.apply(receiver, args)
+    }
+    return {
+        resolve: (data: any, ...args: any[]) => {
+            const hs = receiver.mappedData.get(data)
+            if (hs) call(hs, args);
+            sender.emittedData = data;
+        },
+        subscribe: (key: any, fn: Handler) => {
+            const existing = receiver.mappedData.get(key)
+            if (!existing) receiver.mappedData.set(key, [fn])
+        },
+        unsubscribe: (key: any, fn: Handler|null = null) => {
+            const hs = receiver.mappedData.get(key)
+            if (hs)
+                if (fn) {
+                    receiver.mappedData.set(key, hs.filter(h => h !== fn))
+                }
+                else receiver.mappedData.delete(key)
+        }
+    }
+}
