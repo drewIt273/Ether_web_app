d67696844efc7d3ff01892e8fc1ba32e6d78c3f4
DrewIt273 <instantdrewIt@gmail.com>
2026-06-16 00:45:42 +0100

    Refactor NodeMessageResolver implementation and integrate subscription methods

--- src/runtime/dom/dom.ts
+++ src/runtime/dom/dom.ts
@@ -4,41 +4,36 @@
 
 import {Module} from "@core/module";
 
+class NodeMessageResolver {
+
+    #call = (n: CellOrBlock, h: HandlerList, ...args: any[]) => {
+        for (const fn of h) fn.apply(n, args)
+    }
+
+    resolve(sender: CellOrBlock, receiver: CellOrBlock, data: any, ...args: any[]) {
+        const hs = receiver.mappedData.get(data)
+        if (hs) this.#call(receiver, hs, args)
+        sender.emittedData = data
+    }
+
+    subscribe(node: CellOrBlock, key: any, ...fn: Handler[]) {
+        const existing = node.mappedData.get(key)
+        if (!existing) node.mappedData.set(key, [...fn])
+    }
+
+    unsubscribe(node: CellOrBlock, key: any, fn: Handler|null = null) {
+        const hs = node.mappedData.get(key)
+        if (hs)
+            if (fn) {
+                node.mappedData.set(key, hs.filter(h => h !== fn))
+            }
+            else node.mappedData.delete(key)
+    }
+}
+
 export class DOMInterface extends Module {
 
     constructor() {
         super()
-    }
-}
-
-interface Sender {
-    emittedData?: any
-}
-interface Receiver {
-    mappedData: Map<any, HandlerList>
-}
-
-function NodeMessageResolver(sender: CellOrBlock, receiver: CellOrBlock) {
-    const call = (h: HandlerList, ...args: any[]) => {
-        for (const fn of h) fn.apply(receiver, args)
-    }
-    return {
-        resolve: (data: any, ...args: any[]) => {
-            const hs = receiver.mappedData.get(data)
-            if (hs) call(hs, args);
-            sender.emittedData = data;
-        },
-        subscribe: (key: any, fn: Handler) => {
-            const existing = receiver.mappedData.get(key)
-            if (!existing) receiver.mappedData.set(key, [fn])
-        },
-        unsubscribe: (key: any, fn: Handler|null = null) => {
-            const hs = receiver.mappedData.get(key)
-            if (hs)
-                if (fn) {
-                    receiver.mappedData.set(key, hs.filter(h => h !== fn))
-                }
-                else receiver.mappedData.delete(key)
-        }
     }
 }
