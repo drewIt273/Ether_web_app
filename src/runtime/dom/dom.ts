aa8cfb7eac916a3c3015960baceee5487114e308
DrewIt273 <instantdrewIt@gmail.com>
2026-06-16 16:41:24 +0100

    Updating imports, defining NodeHierachyCheck function, setting onInit and onReady methods, initializing the MutationObserver and useful properties.

--- src/runtime/dom/dom.ts
+++ src/runtime/dom/dom.ts
@@ -3,6 +3,7 @@
  */
 
 import {Module} from "@core/module";
+import {UINodeMap, UICell, UIBlock, UIComponent} from "./ui-root";
 
 class NodeMessageResolver {
 
@@ -33,7 +34,59 @@ class NodeMessageResolver {
 
 export class DOMInterface extends Module {
 
+    observer: MutationObserver
+    nodeMsg: NodeMessageResolver
+    nodelist: Node[]
     constructor() {
         super()
+        this.observer = new MutationObserver(muts => {
+            for (const mut of muts) {
+                if (mut.addedNodes) for (const added of mut.addedNodes) {
+                    if (!UINodeMap.has(added)) this.nodelist.push(added)
+                    else {
+                        let e = NodeHierachyCheck(added)
+                    }
+                }
+                if (mut.removedNodes) for (const removed of mut.removedNodes) {}
+            }
+        })
+        this.nodeMsg = new NodeMessageResolver
+        this.nodelist = []
+        this.observer.observe(document, {childList: true, subtree: true})
+    }
+
+    async onInit() {
+        if (typeof window === 'undefined') return '[DOM] not running in browser environment'  
+        else this.init = !0
+    }
+
+    async onReady() {
+        this.ready = !1
+    }
+}
+
+function NodeHierachyCheck(n: Node) {
+    const o = UINodeMap.get(n), p = n.parentElement; let h;
+    if (p) h = UINodeMap.get(p)
+    if (o && p && h) {
+        const i = hierOrder(o), c = hierOrder(h)
+        if (n.childNodes.length) n.childNodes.forEach(k => NodeHierachyCheck(k))
+        if (i > c) {
+            try {
+                p?.removeChild(n)
+                throw new Error(`NodeHierachyError: ${r(o)} cannot mount ${r(h)}`)
+            }
+            finally {return n}
+        }
+    }
+    else return;
+    function hierOrder(n: UINode) {
+        if (n instanceof UICell) return 1
+        else if (n instanceof UIBlock) return 2
+        else if (n instanceof UIComponent) return 3
+        else return 0
+    }
+    function r(n: UINode) {
+        return `a ${n.node.hasAttribute('ui-cell-id') ? 'UICell' : n.node.hasAttribute('ui-block-id') ? 'UIBlock' : 'UIComponent'}`
     }
 }
