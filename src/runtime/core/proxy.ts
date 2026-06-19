722cbc701741ca8e550b49467d47d95924aa0a08
DrewIt273 <instantdrewIt@gmail.com>
2026-06-19 19:21:15 +0100

    Defining RuntimeProxy, object used by Rune instances to communicate privately with each other. Maybe will be extended to handle communication with a web app's API.

---
+++ src/runtime/core/proxy.ts
@@ -0,0 +1,49 @@
+/**
+ * Instance by DrewIt
+ */
+
+import { ArrayLogLock } from "@assets/registry";
+import { RuntimeProxyError } from "./error";
+
+type ProxyMessageType = 'request'
+
+export type MessageHandler = (msg: ProxyMessage) => void | Promise<void>
+
+export interface ProxyMessage {
+    type: ProxyMessageType
+    msg: any
+}
+
+export const RuneProxies: ArrayLogLock<RuntimeProxy> = new ArrayLogLock()
+
+export class RuntimeProxy {
+
+    target: Rune
+    constructor(target: Rune) {
+        this.target = target
+        this.target.proxies.push(this)
+        RuneProxies.log(this)
+    }
+
+    #mh: MessageHandler | null = null
+
+    /**
+     * Sets a function to be called each time the proxy is used to send messages
+     */
+    onMessage(fn: MessageHandler) {
+        this.#mh = fn
+    }
+
+    send(msg: ProxyMessage) {
+        const o = this.target.proxyInterface
+        if (o.allowed) {
+            // Executes a function to be called (if defined using onMessage) on each use of the proxy
+                if (this.#mh) this.#mh.call(this.target, msg)
+            const p = o.mappedBehavior
+            if (o.onMessage) o.onMessage.call(this.target, msg)
+            if (p.has(msg)) p.get(msg)?.call(this.target)
+            o.received.push(msg)
+        }
+        else throw new RuntimeProxyError(`${this.target} did not allowed the message to be received.`)
+    }
+}
