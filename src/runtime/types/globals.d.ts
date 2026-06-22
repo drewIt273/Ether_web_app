dfadafac9b17702cf58e8b3cdd0940070fd60d97
DrewIt273 <instantdrewIt@gmail.com>
2026-06-22 15:22:26 +0100

    Redefining type UINode.

--- src/runtime/types/globals.d.ts
+++ src/runtime/types/globals.d.ts
@@ -2,15 +2,15 @@
  * Instance by DrewIt
  */
 
-import {UICell as C, UIBlock as B, UIComponent as K, UINode as U} from "@dom/ui-root"
+import {UICell as C, UIBlock as B, UIComponent as K} from "@dom/ui-root"
 import {Rune as R} from "@core/runtime";
 import {DOMInterface as D} from "@dom/dom";
 
 declare global {
-    type UINode = U;
     type UICell = C;
     type UIBlock = B;
     type UIComponent = K;
+    type UINode = C | B | K;
     type CellOrBlock = C | B;
     type Handler = (...args: any[]) => any;
     type HandlerList = Handler[];
