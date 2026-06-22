a3f1b82e73882dd4a11f5c572c401bd33d4c2d10
DrewIt273 <instantdrewIt@gmail.com>
2026-06-22 18:40:38 +0100

    Refactor to imrpove type annotations.

--- src/assets/registry.ts
+++ src/assets/registry.ts
@@ -4,10 +4,11 @@
 
 import {strictObject, isValidJSONString} from "./any";
 
-export class Registry {
-    reg: Record<string, any>
-    constructor(src: {} | Registry = {}) {
-        this.reg = strictObject(src) ? {...src} : {}
+export class Registry<V extends any> {
+    reg: Record<string, V>
+    constructor(src: Record<string, V> | {} = {}) {
+        let o: Record<string, any> = {}
+        this.reg = strictObject(src) ? {...src} : o
     }
 
     #n = (k = '') => {
@@ -34,8 +35,8 @@ export class Registry {
     /**
      * Writes data in the registry and returns it key
      */
-    write(data: any, key = ''): string {
-        if (!(typeof key !== 'string') || !key || key === '') key = this.#generatekey()
+    write(data: V, key = ''): string {
+        if (typeof key !== 'string' || !key) key = this.#generatekey()
         this.reg[key] = data
         return key
     }
@@ -43,7 +44,7 @@ export class Registry {
     /**
      * Calls a callback for each key in the registry
      */
-    forEach(callback: (value: any, key: string) => any) {
+    forEach(callback: (value: V, key: string) => any) {
         Object.entries(this.reg).forEach(([k, v]) => callback.call(this, v, k))
         return this
     }
@@ -51,7 +52,7 @@ export class Registry {
     /**
      * Returns the given value at the specified key
      */
-    get(key: string): any {
+    get(key: string) {
         return this.reg[key]
     }
 
@@ -59,41 +60,41 @@ export class Registry {
      * Removes the specified key from the registry
      */
     remove(key: string) {
-        delete this.reg[key]
+        delete this.reg?.[key]
         return this
     }
 
     /**
      * Returns the key of the specified value o if found in the registry
      */
-    keyOf(o: any): string | undefined {
+    keyOf(o: V): string | undefined {
         for (const [k, v] of Object.entries(this.reg)) if (o === v) return k
     }
 
     /**
      * Returns first value where predicate returns true
      */
-    find(predicate: (v: any, k?: string) => boolean): any {
+    find(predicate: (v: V, k?: string) => boolean): any {
         for (const [k, v] of Object.entries(this.reg)) if (predicate(v, k)) return v
     }
 
     /**
      * Filters entries where predicate returns true
      */
-    filter(predicate: (v: any, k?: string) => boolean): Registry {
-        const filtered: Record<string, any> = {}
+    filter(predicate: (v: V, k?: string) => boolean) {
+        const filtered: Record<string, V> = {}
         Object.entries(this.reg).forEach(([a, b]) => {
             if (predicate(b, a)) filtered[a] = b
         })
-        return new Registry(filtered)
+        return filtered
     }
 
     /**
      * 
      */
-    mutate(predicate: (v: any, k?: string) => boolean) {
+    mutate(predicate: (v: V, k?: string) => boolean) {
         for (const [k, v] of Object.entries(this.reg)) {
-            if (predicate(v, k)) Object.defineProperty(this.reg, k, v)
+            if (predicate(v, k)) this.reg[k] = v
         }
         return this
     }
@@ -101,10 +102,10 @@ export class Registry {
     /**
      * Merges other registries and/or objects into this one
      */
-    merge(...others: [Registry, string][]) {
+    merge(...others: [Registry<any>, string][]) {
         others.forEach(other => {
             const o = other[0], r = other[1], k = this.includesKey(r) ? `_${r}` : r
-            this.reg[k] = o
+            Object.defineProperty(this.reg, k, o.reg)
         })
     }
 
@@ -114,7 +115,7 @@ export class Registry {
      * @param deleteCount The number of elements to remove. If value of this argument is either a negative number, zero, undefined, or a type that cannot be converted to an integer, the function will evaluate the argument as zero and not remove any elements.
      * @param items  Elements to insert into the array in place of the deleted elements.
      */
-    splice(start: number, deleteCount: number, ...items: [string, any][]): [string, any][] {
+    splice(start: number, deleteCount: number, ...items: any[]) {
         return Object.entries(this.reg).splice(start, deleteCount, ...items)
     }
 
@@ -135,7 +136,7 @@ export class Registry {
     /**
      * Converts a valid JSON string into a registry. Returns undefined if src is invalid.
      */
-    parse(src: string): Registry | undefined {
+    parse(src: string): Registry<any> | undefined {
         if (isValidJSONString(src)) return new Registry(JSON.parse(src))
     }
 
