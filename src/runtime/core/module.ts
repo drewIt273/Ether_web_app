39c1125efa9d7934aa13afcf0410dee64a62fa70
DrewIt273 <instantdrewIt@gmail.com>
2026-06-28 17:01:43 +0100

    Refactor ModuleMessageResolver to use generics for type safety and improve the resolve method implementation. Update subscribe and unsubscribe methods to correctly reference IMC mappedData. Implement IMC in Module for emitting mapping and unmapping data. Implement interface EventsModuleData and type ModulesMappedData for type safety.

--- src/runtime/core/module.ts
+++ src/runtime/core/module.ts
@@ -3,70 +3,77 @@
  */
 
 interface ModuleMessageResolver {
-    resolve(sender: Module, receiver: Module, data: any, ...args: any[]): void
+    resolve<K extends keyof ModulesMappedData>(sender: Module, receiver: Module, data: K, args: ModulesMappedData[K]): void
     subscribe(n: Module, key: any, ...fn: Handler[]): void
     unsubscribe(n: Module, key: any, fn?: Handler|null): void
 }
 
-function call(n: Module, h: HandlerList, ...args: any[]) {
-    for (const fn of h) fn.apply(n, args)
-}
-
 const IMC: ModuleMessageResolver = {
-    resolve(sender: Module, receiver: Module, data: any, ...args: any[]) {
-        const hs = receiver.mappedData.get(data)
-        if (hs) call(receiver, hs, ...args)
-        sender.emittedData = data
+    resolve(sender, receiver, data, args) {
+        const hs = receiver.IMC.mappedData.get(data)
+        if (hs) for (const fn of hs) fn.apply(receiver, args)
+        sender.IMC.emittedData = receiver.IMC.receivedData = data
     },
-    subscribe(n: Module, key: any, ...fn: Handler[]) {
-        const existing = n.mappedData.get(key)
-        if (!existing) n.mappedData.set(key, [...fn])
+
+    subscribe(n, key, ...fn) {
+        const existing = n.IMC.mappedData.get(key)
+        if (!existing) n.IMC.mappedData.set(key, [...fn])
         else existing.push(...fn)
     },
-    unsubscribe(n: Module, key: any, fn: Handler|null = null) {
-        const hs = n.mappedData.get(key)
+
+    unsubscribe(n, key, fn: Handler|null = null) {
+        const hs = n.IMC.mappedData.get(key)
         if (hs)
             if (fn) {
-                n.mappedData.set(key, hs.filter(h => h !== fn))
+                n.IMC.mappedData.set(key, hs.filter(h => h !== fn))
             }
-            else n.mappedData.delete(key)
+            else n.IMC.mappedData.delete(key)
     }
+}
+
+interface MsgResolverUnit {
+    emittedData: any
+    receivedData: any
+    mappedData: Map<any, HandlerList>
+    map: (data: any, ...fn: HandlerList) => MsgResolverUnit
+    unmap: (data: any, fn?: Handler | null) => MsgResolverUnit
+    emit<K extends keyof ModulesMappedData> (data: K, to: Module, args: ModulesMappedData[K]): MsgResolverUnit
 }
 
 export class Module {
 
+    readonly IMC: MsgResolverUnit
     init: boolean
     ready: boolean
-    sentData: any
-    receivedData: any
-    emittedData: any
-    mappedData: Map<any, HandlerList>
     rune: Rune
     constructor(r: Rune) {
         this.rune = r
         this.init = !1
         this.ready = !1
-        this.mappedData = new Map
-    }
-
-    emit(data: any) {
-        const o = {
-            to: (n: Module) => {
-                IMC.resolve(this, n, data)
-                return o
-            },
-            /**
-             * Returns emit.map for continues mapping
-             */
-            map: (...fn: HandlerList) => {
+        this.IMC = {
+            mappedData: new Map(),
+            receivedData: null,
+            emittedData: null,
+            map: (data, ...fn: Handler[]) => {
                 IMC.subscribe(this, data, ...fn)
-                return o.map
+                return this.IMC
             },
-            unmap: (fn: Handler|null) => {
+            unmap: (data, fn: Handler | null = null) => {
                 IMC.unsubscribe(this, data, fn)
-                return o
+                return this.IMC
+            },
+            emit: (data, to: Module, args) => {
+                IMC.resolve(this, to, data, args)
+                return this.IMC
             }
         }
-        return o
     }
 }
+
+interface EventsModuleData {
+    'ln': [keyof DocumentEventMap, Node, ((ev: Event) => void)[]]
+    'un': [Node, keyof DocumentEventMap | null]
+    'kc': [string[], Node, Handler]
+}
+
+type ModulesMappedData = EventsModuleData
