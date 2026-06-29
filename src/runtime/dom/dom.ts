5a8b802012207f9977482a0746c98113d49fefae
DrewIt273 <instantdrewIt@gmail.com>
2026-06-29 16:37:15 +0100

    Refactor DOMInterface to implement GlobalEvents and GlobalStates for improved event handling and state management

--- src/runtime/dom/dom.ts
+++ src/runtime/dom/dom.ts
@@ -12,6 +12,18 @@ interface NodeMessageResolver {
     unsubscribe(node: CellOrBlock, key: any, fn?: Handler|null): void
 }
 
+interface UiEventsInterface {
+    onEvent(ev: keyof DocumentEventMap, node: Node, ...handlers: ((ev: Event) => void)[]): void
+    unEvent(node: Node, ev?: keyof DocumentEventMap | null): void
+    keyEvent(node: Node, keys: string[], handler: ((ev: Event) => void)): void
+}
+
+interface UiStatesInterface {
+    setState(node: UINode, state: string, opts: {schedule: boolean}): void
+    defineState(node: UINode, state: string, call: Handler): void
+    defineCompute(node: UINode, state: string, call: Handler): void
+}
+
 export class DOMInterface extends Module {
 
     observer: MutationObserver
@@ -29,12 +41,12 @@ export class DOMInterface extends Module {
                     if (!o) this.nodelist.push(added)
                     else {
                         o.meta.belongsTo = this
-                        o.meta.onEventMap.forEach((v, k) => this.onEvent(k, o.node, ...v)), o.meta.onEventMap.clear()
+                        o.meta.onEventMap.forEach((v, k) => this.GlobalEvents.onEvent(k, o.node, ...v)), o.meta.onEventMap.clear()
                     }
                 }
                 if (mut.removedNodes) for (const removed of mut.removedNodes) {
                     const o = UINodeMap.get(removed)
-                    this.unEvent(removed)
+                    this.GlobalEvents.unEvent(removed)
                     if (!o) {}
                     else {
                         o.meta.unEventSet.clear()
@@ -97,16 +109,32 @@ export class DOMInterface extends Module {
         else throw new DOMInterfaceError(`Node ${into} out of reach`)
     }
 
-    onEvent(ev: keyof DocumentEventMap, node: Node, ...handlers: ((ev: Event) => void)[]) {
-        this.#ne(node, () => this.IMC.emit('ln', this.rune.events, [ev, node, handlers]))
+    GlobalEvents: UiEventsInterface = {
+        onEvent: (ev: keyof DocumentEventMap, node: Node, ...handlers: ((ev: Event) => void)[]) => {
+            this.#ne(node, () => this.IMC.emit('ln', this.rune.events, [ev, node, handlers]))
+        },
+
+        unEvent: (node: Node, ev: keyof DocumentEventMap | null = null) => {
+            this.#ne(node, () => this.IMC.emit('un', this.rune.events, [node, ev]))
+        },
+
+        keyEvent: (node: Node, keys: string[], handler: ((ev: Event) => void)) => {
+            this.#ne(node, () => this.IMC.emit('kc', this.rune.events, [keys, node, handler]))
+        },
     }
 
-    unEvent(node: Node, ev: keyof DocumentEventMap | null = null) {
-        this.#ne(node, () => this.IMC.emit('un', this.rune.events, [node, ev]))
-    }
+    GlobalStates: UiStatesInterface = {
+        setState: (node: UINode, state: string, opts: {schedule: boolean}) => {
+            this.#se(node, () => this.IMC.emit('set', this.rune.states, [node, state, opts]))
+        },
 
-    keyEvent(node: Node, keys: string[], handler: ((ev: Event) => void)) {
-        this.#ne(node, () => this.IMC.emit('kc', this.rune.events, [keys, node, handler]))
+        defineState: (node: UINode, state: string, call: Handler) => {
+            this.#se(node, () => this.IMC.emit('df', this.rune.states, [node, state, call]))
+        },
+
+        defineCompute: (node: UINode, state: string, call: Handler) => {
+            this.#se(node, () => this.IMC.emit('dc', this.rune.states, [node, state, call]))
+        }
     }
 
     #ne(node: Node, emit: Handler) {
@@ -121,6 +149,21 @@ export class DOMInterface extends Module {
                 })
             }
             else throw OutOfReachError(node)
+        }
+    }
+
+    #se(node: UINode, emit: Handler) {
+        if (this.root.contains(node.node)) emit()
+        else {
+            const r = node.meta.belongsTo?.rune
+            if (r) {
+                const o = this.rune.proxyInterface.send({type: 'uiState', msg: node.node}, r)
+                o?.then(v => {
+                    if (v?.type === 'rejected') throw OutOfReachError(node.node)
+                    else emit()
+                })
+            }
+            else throw OutOfReachError(node.node)
         }
     }
 }
