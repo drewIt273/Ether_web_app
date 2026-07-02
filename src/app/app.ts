a1c4b56f2e0f85dbf4fdcb5c2b46884e7f7b128c
DrewIt273 <instantdrewIt@gmail.com>
2026-07-02 17:38:00 +0100

    Add app.ts, the entry point for Rune, for Ether.

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+/**
+ * Instance by DrewIt
+ */
+
+import {Rune} from "@core/rune";
+
+const rune = new Rune(), a = rune.boot()
+
+if (a instanceof Error) throw new Error();
+
+export const dom = rune.dom, scheduler = rune.scheduler
