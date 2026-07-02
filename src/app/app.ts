99a96f800fbae3bc480a629a2e1ec5fa21ea69f9
DrewIt273 <instantdrewIt@gmail.com>
2026-07-02 17:43:06 +0100

    Fix error handling in app.ts to throw the original Error object

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,6 @@ import {Rune} from "@core/rune";
 
 const rune = new Rune(), a = rune.boot()
 
-if (a instanceof Error) throw new Error();
+if (a instanceof Error) throw a;
 
 export const dom = rune.dom, scheduler = rune.scheduler
