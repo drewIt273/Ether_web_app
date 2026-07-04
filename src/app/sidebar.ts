b87ae336f084a2a9a76a8fa10cc06ca2a44945b4
DrewIt273 <instantdrewIt@gmail.com>
2026-07-04 19:08:44 +0100

    Refactor sidebar component: Enhance structure and event handling for improved functionality

--- src/app/sidebar.ts
+++ src/app/sidebar.ts
@@ -2,31 +2,42 @@
  * Instance by DrewIt
  */
 
-import {UIComponent, newnode} from "@dom/ui-root";
+import {UIComponent, UIBlock, newnode} from "@dom/ui-root";
+import {onResizeX} from "./ui";
+import { dom } from "./boot";
 
-const sidebar = new UIComponent({
+const o = new UIComponent({
     tag: 'aside',
     attrs: {
         expand: ''
     },
     append: [
-        newnode('div', {
-            className: 'col-resizer right-side',
-            on: ['click', () => {
-                console.log('It seems to work!')
-            }]
-        })
+        () => newnode('div', {
+            className: 'p-xxs relative h-full',
+            append: [
+                () => newnode('div', {
+                    className: 'h-center p-sm'
+                }),
+                new UIBlock({}),
+            ]
+        }),
+        () => newnode('div', {className: 'col-resizer right-side'})
     ],
     UIKey: 'sidebar',
 });
 
-sidebar.on('append', () => {
-    sidebar.defineState('relative')('relative')
-    sidebar.setState('relative')
+const d = newnode('div', {
+    className: 'sidebar-con'
+}); d.append(o.node)
+
+o.on('append', () => {
+    o.init()
+    o.defineState('relative', () => {d.removeAttribute('collapse'); dom.GlobalEvents.unEvent(d, 'mouseleave')})('absolute', () => {d.setAttribute('collapse', ''); dom.GlobalEvents.onEvent('mouseenter', d, () => o.style({left: '0'})); dom.GlobalEvents.onEvent('mouseleave', d, () => o.style({left: '-380px'}))})
+    o.setState('relative')
+    let r = o.find('.col-resizer.right-side');
+    o.delegate(r, 'pointerdown', onResizeX(r, o.node, {max: 370, min: 300}) as () => void)
 })
 
-const fn = () => sidebar
+const sidebar$ = () => d
 
-export {fn}
-
-console.log(Date.now())
+export {sidebar$}
