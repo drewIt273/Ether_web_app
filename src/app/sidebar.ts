c4c79c9c1667a23664cfa95f918b480c6bdf3cfb
DrewIt273 <instantdrewIt@gmail.com>
2026-07-05 15:44:08 +0100

    Enhance sidebar functionality: Add toggle state management and refine event handling for mouse interactions

--- src/app/sidebar.ts
+++ src/app/sidebar.ts
@@ -3,7 +3,7 @@
  */
 
 import {UIComponent, UIBlock, newnode} from "@dom/ui-root";
-import {onResizeX} from "./ui";
+import {onResizeX, toggleUINodeState} from "./ui";
 import { dom } from "./boot";
 
 const o = new UIComponent({
@@ -21,7 +21,7 @@ const o = new UIComponent({
                 new UIBlock({}),
             ]
         }),
-        () => newnode('div', {className: 'col-resizer right-side'})
+        ($) => newnode('div', {className: 'col-resizer right-side'})
     ],
     UIKey: 'sidebar',
 });
@@ -32,10 +32,11 @@ const d = newnode('div', {
 
 o.on('append', () => {
     o.init()
-    o.defineState('relative', () => {d.removeAttribute('collapse'); dom.GlobalEvents.unEvent(d, 'mouseleave')})('absolute', () => {d.setAttribute('collapse', ''); dom.GlobalEvents.onEvent('mouseenter', d, () => o.style({left: '0'})); dom.GlobalEvents.onEvent('mouseleave', d, () => o.style({left: '-380px'}))})
+    o.defineState('absolute', () => {d.setAttribute('collapse', ''); dom.GlobalEvents.onEvent('mouseenter', d, () => o.style({left: '10px'})); dom.GlobalEvents.onEvent('mouseleave', d, () => o.style({left: '-380px'}))})('relative', () => {d.removeAttribute('collapse'); dom.GlobalEvents.unEvent(d, 'mouseenter'); dom.GlobalEvents.unEvent(d, 'mouseleave'); o.style({left: 0})})
     o.setState('relative')
     let r = o.find('.col-resizer.right-side');
     o.delegate(r, 'pointerdown', onResizeX(r, o.node, {max: 370, min: 300}) as () => void)
+    dom.GlobalEvents.onEvent('click', r, () => toggleUINodeState(o, 'absolute', 'relative'))
 })
 
 const sidebar$ = () => d
