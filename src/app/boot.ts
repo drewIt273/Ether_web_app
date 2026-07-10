4d6c4f4c58e2cc06672301a2a7f65cf03b1cd81a
DrewIt273 <instantdrewIt@gmail.com>
2026-07-10 17:31:20 +0100

    nothing important.

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -4,23 +4,23 @@
 
 import {Rune} from "@core/rune";
 
-const nodeRuneData = new WeakMap<Node, { id: string; isRuneRoot: boolean }>();
+const n = new WeakMap<Node, { id: string; isRuneRoot: boolean }>();
 
 Object.defineProperty(Node.prototype, 'rune', {
     get() {
-        if (!nodeRuneData.has(this)) {
-            nodeRuneData.set(this, { id: '', isRuneRoot: false });
+        if (!n.has(this)) {
+            n.set(this, { id: '', isRuneRoot: false });
         }
-        return nodeRuneData.get(this)!;
+        return n.get(this)!;
     },
     set(value) {
         if (typeof value === 'object' && value !== null) {
-            nodeRuneData.set(this, value as { id: string; isRuneRoot: boolean });
+            n.set(this, value as { id: string; isRuneRoot: boolean });
         }
     },
     configurable: true,
     enumerable: false,
-});
+})
 
 const rune = new Rune(), a = rune.boot()
 
