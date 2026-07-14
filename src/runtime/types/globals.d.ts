679e91334427db86f1ca50545932a93b0a36a062
DrewIt273 <instantdrewIt@gmail.com>
2026-07-14 19:07:16 +0100

    Refactor globals.d.ts to streamline imports and remove unused type definitions for improved clarity

--- src/runtime/types/globals.d.ts
+++ src/runtime/types/globals.d.ts
@@ -2,19 +2,11 @@
  * Instance by DrewIt
  */
 
-import {UICell as C, UIBlock as B, UIComponent as K, U, F} from "@dom/ui-root"
-import {Rune as R} from "@core/rune";
-import {DOMInterface as D} from "@dom/dom";
-import {G} from "@core/events";
+import {U, F} from "@dom/ui-root"; import {Rune as R} from "@core/rune"; import {D} from "@dom/dom"; import {G} from "@core/events";
 
 declare global {
-    type UICell = C;
-    type UIBlock = B;
-    type UIComponent = K;
     type UINode = U;
-    type CellOrBlock = C | B;
     type Handler = (...args: any[]) => any;
-    type HandlerList = Handler[];
     type Rune = R
     type DOMInterface = D
     type GlobalEvents = G
