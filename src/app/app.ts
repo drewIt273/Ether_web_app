f162c67f567cc33e8394943542c10050dea14855
DrewIt273 <instantdrewIt@gmail.com>
2026-07-19 21:36:54 +0100

    Make rune.boot() asynchronous to fix bug: The if statement below it was running before the runtime fully booted.

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,8 +6,9 @@ import {Rune} from "@core/rune";
 import {Sidebar$} from "./SidebarLayout";
 import {main$} from "./MainLayout";
 
-const rune = new Rune(), a = rune.boot()
+const rune = new Rune(), a = await rune.boot()
 if (a instanceof Error) throw a;
+
 export const dom = rune.dom, scheduler = rune.scheduler
 
 if (dom.ready) dom.append(Sidebar$())(main$())
