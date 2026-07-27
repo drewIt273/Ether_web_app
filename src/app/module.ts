009a9862ad294dad62f5b1ff8b46d2acb08ce555
DrewIt273 <instantdrewIt@gmail.com>
2026-07-27 21:39:14 +0100

    Refactor module.ts: Redefine UiModule and UiModulesInterfaceMap; enhance UiComponent and UiConstructor for improved module management

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,14 +2,30 @@
  * Instance by DrewIt
  */
 
-interface UiModules {
-    sidebar?: UiComponent
-    [x: string]: UiComponent
+interface UiModule {
+    readonly name: keyof UiModulesInterfaceMap
+    root: UiComponent | null
+    readonly imports: string[]
+    onImport: Handler | null
+}
+
+interface UiModulesInterfaceMap {
+    "sidebar"?: UiModule
+    "issues"?: UiModule
+    "projects"?: UiModule
+}
+
+const modules: UiModulesInterfaceMap = {}
+
+const Imports = {
+    sidebar: () => import('@app/sidebar/index'),
+    isses: () => import('@app/issues/index'),
+    projects: () => import('@app/projects/index')
 }
 
 class UiComponent {
 
-    constructor(n: () => Node) {
+    constructor(n: () => HTMLElement) {
         this.#fn = n
     }
 
@@ -18,21 +34,112 @@ class UiComponent {
         return this.#n
     }
 
-    readonly mounted = this.node.$.mounted
+    readonly name: string = ''
+    readonly module: keyof UiModulesInterfaceMap | null = null
 
-    #n?: Node
-    #fn: () => Node
-}
+    deps: string[] = []
 
-class ui {
-
-    static expose<K extends keyof UiModules>(key: K, node: HTMLElement) {
-        const u = new UiComponent(() => node);
-        this.modules[key] = u
-        return u
+    mount(n: HTMLElement | keyof UiModulesInterfaceMap, asOnlyNode: boolean = false) {
+        if (this.module) this.deps.forEach(async d => await ui.require(`${this.module as keyof UiModulesInterfaceMap}:${d}`))
+        this.o.mount?.()
     }
 
-    static readonly modules: UiModules = {}
+    unmount() {
+        const n = this.node.parentElement
+        if (n) n.removeChild(this.node)
+        this.o.unmount?.()
+    }
+
+    get mounted() {
+        return this.node.$.mounted
+    }
+
+    o: {
+        mount: Handler | undefined
+        unmount: Handler | undefined
+    } = {
+        mount: () => {},
+        unmount: () => {}
+    }
+
+    #n?: HTMLElement
+    #fn: () => HTMLElement
 }
 
-export {ui}
+interface ModuleDefinitionObject {
+    root?: HTMLElement | null
+    imports?: string[]
+    onMount?: Handler
+    onUnmount?: Handler
+    onImport?: Handler
+}
+
+class UiModule {
+
+    root: UiComponent | null
+    constructor(name: keyof UiModulesInterfaceMap, root: UiComponent | null = null) {
+        this.#n = name
+        this.root = root // @ts-expect-error
+        this.root?.module = name
+    }
+
+    #n: keyof UiModulesInterfaceMap
+
+    // @ts-expect-error
+    readonly name = this.#n
+
+    readonly imports: string[] = []
+
+    onImport: Handler | null = null
+}
+
+class UiConstructor {
+
+    static define<K extends keyof UiModulesInterfaceMap>(name: K, props: ModuleDefinitionObject): UiModulesInterfaceMap[K] {
+        const u = props.root ? new UiComponent(() => props.root as HTMLElement) : null, o = new UiModule(name, u)
+        o.onImport = props.onImport ?? null
+        if (o.root?.o) o.root.o = {mount: props.onMount, unmount: props.onUnmount}
+        this.modules[name] = o
+        return o
+    }
+
+    static async require<K extends keyof UiModulesInterfaceMap, L extends string>(key: K | `${K}:${L}`): Promise<UiModulesInterfaceMap[K]> {
+        let o = this.modules[key as K]
+        if (key.match(/^[a-z][a-z0-9_-]*:[a-z][a-z0-9_-]*$/i)) {
+            const s = key.split(':'), a = await import(`@app/${s[0]}/${s[1]}`)
+        }
+        else {
+            // @ts-expect-error
+            const a: {module: UiModule} = await Imports[key]()
+            a.module.onImport?.()
+        }
+        return o
+    }
+
+    static readonly modules: UiModulesInterfaceMap = modules
+
+    static defineProperty<K extends keyof UiModulesInterfaceMap>(key: K, property: string, value: any) {
+        const u = this.modules[key]
+        if (u) {
+            if (u[property as keyof UiModule] === undefined) {
+                Object.defineProperty(u, property, {
+                    value: value,
+                    enumerable: false,
+                    configurable: false
+                })
+            }
+            else throw new Error(`Cannot overwrite already defined property of UiModule ${u.name}`)
+        }
+        else return;
+    }
+}
+
+export const ui: UiConstructor = UiConstructor
+
+interface UiConstructor {
+    require<K extends keyof UiModulesInterfaceMap>(key: K): Promise<UiModulesInterfaceMap[K] | undefined>
+    require<K extends keyof UiModulesInterfaceMap, L extends string>(key: `${K}:${L}`): Promise<UiComponent | undefined>
+    defineProperty<K extends keyof UiModulesInterfaceMap>(key: K, property: string, value: any): void
+    define<K extends keyof UiModulesInterfaceMap>(name: K, props: ModuleDefinitionObject): UiModulesInterfaceMap[K]
+    readonly modules: UiModulesInterfaceMap
+}
