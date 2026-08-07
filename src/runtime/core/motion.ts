20e14f5ced5c2ed41c440784ae412d6f8d1d62f2
DrewIt273 <instantdrewIt@gmail.com>
2026-08-07 19:34:24 +0100

    Refactor UiMotionEngine: Update animate method and enhance play functionality for improved animation handling

--- src/runtime/core/motion.ts
+++ src/runtime/core/motion.ts
@@ -18,6 +18,7 @@ export class UiMotionEngine extends Module {
 
     constructor(r: Rune) {
         super(r)
+        this.IMC.map('anim', (node: HTMLElement, keyframes: MotionFrame[] | null, options: UiMotionOptions) => this.animate(node, keyframes, options))
     }
 
     async onInit() {
@@ -28,8 +29,19 @@ export class UiMotionEngine extends Module {
         this.ready = !0
     }
 
-    async animate(node: HTMLElement, keyframes: MotionFrame[] | null, options: number | KeyframeAnimationOptions | undefined = undefined) {
-        return node.$.motion.current = (() => node.animate(keyframes, options))()
+    animate(node: HTMLElement, keyframes: MotionFrame[] | null, options: UiMotionOptions) {
+        const k = new KeyframeEffect(node, keyframes, options)
+        return new Animation(k)
+    }
+
+    play(node: Node, motion: Animation | string) {
+        const m = node.$.motion, a = typeof motion === 'string' ? m.defs.get(motion) : motion
+        if (a) {
+            if (m.current) m.current.finish()
+            a.play(), m.current = a, m.state = 'running';
+            let fn = () => {m.current = null, m.state = 'null'; a.removeEventListener('finish', fn)}
+            a.addEventListener('finish', fn)
+        }
     }
 }
 
@@ -40,4 +52,5 @@ declare global {
         composite?: CompositeOperationOrAuto
         offset?: number | null
     }
+    type UiMotionOptions = number | KeyframeAnimationOptions | undefined
 }
