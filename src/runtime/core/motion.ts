a5c88c3a95888007d1b6dc111390e905dac5ffca
DrewIt273 <instantdrewIt@gmail.com>
2026-08-10 21:39:40 +0100

    Enhance UiMotionEngine: Add frames property to Animation and improve play method handling

--- src/runtime/core/motion.ts
+++ src/runtime/core/motion.ts
@@ -14,14 +14,44 @@ interface AnimatableCSSProperties {
     backgroundColor?: `#${string}`
 }
 
+interface playOptions {
+    action: 'forward' | 'reverse'
+    commit: boolean
+}
+
+interface AnimationEventInterface {
+    readonly record: WeakMap<Animation, Record<keyof AnimationEventMap, Handler>>
+    set(key: Animation, ev: keyof AnimationEventMap, fn: Handler): void
+}
+
+const ei: AnimationEventInterface = {
+    record: new WeakMap(),
+    set(key, ev, fn) {
+        const o = {[ev]: fn} as Record<keyof AnimationEventMap, Handler>
+        this.record.set(key, Object.assign(this.record.get(key) ?? {}, o))
+    },
+}
+
+const sym = Symbol('MotionFrames')
+
 export class UiMotionEngine extends Module {
 
     constructor(r: Rune) {
         super(r)
-        this.IMC.map('anim', (node: HTMLElement, keyframes: MotionFrame[] | null, options: UiMotionOptions) => this.animate(node, keyframes, options))
+        this.IMC.map('anim', (node: HTMLElement, keyframes: MotionFrame[], options: UiMotionOptions) => this.animate(node, keyframes, options))
     }
 
     async onInit() {
+        Object.defineProperty(Animation.prototype, 'frames', {
+            set(v) {
+                if (Array.isArray(v)) this[sym] = v
+            },
+            get() {
+                return this[sym]
+            },
+            configurable: false,
+            enumerable: false
+        })
         this.init = !0
     }
 
@@ -29,20 +59,50 @@ export class UiMotionEngine extends Module {
         this.ready = !0
     }
 
-    animate(node: HTMLElement, keyframes: MotionFrame[] | null, options: UiMotionOptions) {
-        const k = new KeyframeEffect(node, keyframes, options)
-        return new Animation(k)
+    animate(node: HTMLElement, keyframes: MotionFrame[], options: UiMotionOptions) {
+        const k = new KeyframeEffect(node, keyframes, options), a = new Animation(k)
+        a.frames = keyframes
+        return a
     }
 
-    play(node: Node, motion: Animation | string) {
+    play(node: Node, motion: Animation | string, options: playOptions = {action: 'forward', commit: true}) {
         const m = node.$.motion, a = typeof motion === 'string' ? m.defs.get(motion) : motion
         if (a) {
-            if (m.current) m.current.finish()
-            a.play(), a.commitStyles(), m.current = a, m.state = 'running';
-            let fn = () => {m.current = null, m.state = 'null'; a.removeEventListener('finish', fn)}
-            a.addEventListener('finish', fn)
+            const frs = a.frames
+            if (m.current) {
+                m.current.pause()
+            }
+            options.action === 'forward' ? a.play() : a.reverse(), m.current = a, m.state = 'running';
+            let fn = () => {
+                if (m.current === a) {
+                    m.current = null, m.state = 'null'
+                }
+                if (options.commit) a.commitStyles()
+            }
+            ei.record.has(a) ? ei.record.get(a)?.finish() : (a.addEventListener('finish', fn), ei.set(a, 'finish', fn))
         }
     }
+}
+
+function persistComputedStyles(n: Node, frames: MotionFrame[]) {
+    for (const f of frames) {
+        for (const [k, v] of Object.entries(f)) {
+            n.$.motion.buffer = {}
+            if (Object.hasOwn(CSSStyleProperties.prototype, k)) n.$.motion.buffer[k as keyof CSSStyleProperties] = `${v}`
+        }
+    }
+    return frames
+}
+
+function constructFrames(n: Node, frames: MotionFrame[]) {
+    for (const f of frames) {
+        for (const [k, v] of Object.entries(f)) {
+            if (k === 'height') { // @ts-expect-error
+                if (v === 'fit-content') f[k] = `${getComputedStyle(n as Element).height}px`
+            }
+        }
+    }
+    return frames
 }
 
 declare global {
@@ -53,4 +113,7 @@ declare global {
         offset?: number | null
     }
     type UiMotionOptions = number | KeyframeAnimationOptions | undefined
+    interface Animation {
+        frames: MotionFrame[]
+    }
 }
